761efa06887304b5066baef2f94eb3a1fabc4724
pabloglezr8 <pabligoru99@gmail.com>
2024-10-15 12:48:39 +0200

    Programamos el inicio de sesión

--- app/src/main/java/com/example/pablo_gonzalez_ruiz_pmdm_tarea1/LogIn.java
+++ app/src/main/java/com/example/pablo_gonzalez_ruiz_pmdm_tarea1/LogIn.java
@@ -1,6 +1,12 @@
 package com.example.pablo_gonzalez_ruiz_pmdm_tarea1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +21,31 @@ public class LogIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextPswrd = findViewById(R.id.editTextPswrd);
+        Button btnLogIn = findViewById(R.id.btnLogIn);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editTextName.getText().toString().equals("admin") && editTextPswrd.getText().toString().equals("admin")){
+                    Intent iniciarSesion = new Intent(LogIn.this, LogInCorrect.class);
+                    startActivity(iniciarSesion);
+                }else {
+                    //se crea  un toast par indicar q los campos son incorrectos
+                    Toast.makeText(LogIn.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
     }
 }
